80605baaaa884e3e9d36d25ad289a209da7b9205
Mohamed SERIK <serikmohamed15@gmail.com>
2021-02-06 23:44:56 +0100

    SQL-PLUS

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
-### Utilisation du Makefile
+Etapes Pour executer le projet :
+
+Etape 1/ creation des tables avec le fichier  :   creation.sql
+
+Etape 2/ inserion dans les tables avec le fichier : insertion.sql
 
 
-1/	Compiler le programme:      make
-
-2/	executer le programe :      ./mst <population>  
-	exemple:
-				./mst 250000
-
-
-3/	Dessiner le tracé le plus optimise sur un graphique :  make draw 
- 	note: Vous devez avoir utiliser le programme 'mst' *avec succes* avant de faire cette commande
-
- 4/	Supprimer tous les fichiers objets (.o), le programme 'mst', le fichier 'result.png' et tous les fichiers output : make fclean
+Etape 3/ lancer le script des requetes le fichier   : req_projet.sql
---
+++ insertion.sql
@@ -0,0 +1,188 @@
+
+
+insert into SERIE values ('Sur ecoute',2002,'Etats-Unis','05/12/2002');
+insert into SERIE values ('Game of Thrones',2011,'Etats-Unis','05/10/2011');
+insert into SERIE values ('Breaking Bad',2008,'Etats-Unis','16/01/2008');
+insert into SERIE values ('Les Soprano',1999 ,'Anglaitaire','03/06/1999');
+insert into SERIE values ('Big Bang Theory', 2007 ,'Etats-Unis','27/09/2007');
+insert into SERIE values ('House of Cards', 2013 ,'Japon','05/05/2013');
+
+
+insert into PERSONNE values ('Alexis','Lepris');
+insert into PERSONNE values ('Juliette','Levant');
+insert into PERSONNE values ('Mathieu','Delaporte');
+insert into PERSONNE values ('Alexandre','De La P(FKatelliere ');
+insert into PERSONNE values (' Jerome ','Rebotier');
+insert into PERSONNE values (' Demitri ','Rassam');
+insert into PERSONNE values (' Serge ','Poucque  ');
+insert into PERSONNE values (' Adrien ','Politowski');
+insert into PERSONNE values (' Gilles ','Waterkeyn ');
+insert into PERSONNE values (' Bernard ',' Murat ');
+insert into PERSONNE values (' Atome ',' Soumache ');
+insert into PERSONNE values (' Aaron ',' Paul ');
+insert into PERSONNE values (' Bryan ',' Cranston ');
+insert into PERSONNE values (' RJ ',' Mitte ');
+insert into PERSONNE values (' Dean',' Norris ');
+insert into PERSONNE values (' Anna ',' Gunn ');
+insert into PERSONNE values (' Besty ',' Brandt ');
+insert into PERSONNE values (' Jonathan ',' Banks ');
+insert into PERSONNE values (' Bob ',' Odenkirk ');
+insert into PERSONNE values (' Charles ',' Baker ');
+insert into PERSONNE values (' Jesse ',' Plemons ');
+insert into PERSONNE values (' Matt L. ',' Jones ');
+insert into PERSONNE values (' Giancarlo ',' Esposito ');
+insert into PERSONNE values (' Kristen ',' Ritter ');
+insert into PERSONNE values (' Steven ',' Michael ');
+insert into PERSONNE values (' Laura  ',' Fraser ');
+insert into PERSONNE values (' Kevin ',' Rankin ');
+insert into PERSONNE values (' Carmen ',' Cirano ');
+insert into PERSONNE values (' David ',' Costabile ');
+insert into PERSONNE values (' Vince ','Gelligan ');
+insert into PERSONNE values (' Bryan ','Cranston ');
+insert into PERSONNE values (' Steven ','Spielberg ');
+
+
+
+
+INSERT INTO CREATEURS VALUES ('Alexis','Lepris','Game of Thrones');
+INSERT INTO CREATEURS VALUES (' Demitri ','Rassam','Game of Thrones');
+INSERT INTO CREATEURS VALUES (' Atome ',' Soumache ','Breaking Bad');
+INSERT INTO CREATEURS VALUES (' Demitri ','Rassam','Les Soprano');
+INSERT INTO CREATEURS VALUES (' Vince ','Gelligan ','Breaking Bad');
+
+
+INSERT INTO UTILISATEURS VALUES ('Rose Lena','11/02/2011 ',23,'F');
+INSERT INTO UTILISATEURS VALUES ('Abde ','15/11/2010', 30 ,'M');
+INSERT INTO UTILISATEURS VALUES (' Msw','13/10/2016 ', 21 ,'F');
+INSERT INTO UTILISATEURS VALUES (' Fati Brahimi','16/12/2013 ', 22 ,'F');
+INSERT INTO UTILISATEURS VALUES (' Kaina Fato','14/01/2012 ',25  ,'F');
+INSERT INTO UTILISATEURS VALUES ('Lea Lon ','13/05/2009 ', 24 ,'F');
+INSERT INTO UTILISATEURS VALUES ('Cooper','11/08/2008 ', 25 ,'M');
+INSERT INTO UTILISATEURS VALUES ('Azrod95','11/08/2008 ', 25 ,'M');
+
+INSERT INTO GENRE VALUES (' Comique');
+INSERT INTO GENRE VALUES (' Drammatique ');
+INSERT INTO GENRE VALUES (' Action ');
+INSERT INTO GENRE VALUES ('Sience fiction');
+INSERT INTO GENRE VALUES ('Comedie');
+INSERT INTO GENRE VALUES (' Animation');
+INSERT INTO GENRE VALUES (' Fantastique');
+INSERT INTO GENRE VALUES ('Soap');
+
+INSERT INTO G_SERIE VALUES ('Comedie','Sur ecoute');
+INSERT INTO G_SERIE VALUES ('Sience fiction','Sur ecoute');
+INSERT INTO G_SERIE VALUES (' Action ','Breaking Bad');
+INSERT INTO G_SERIE VALUES ('Soap','Les Soprano');
+INSERT INTO G_SERIE VALUES (' Animation','Big Bang Theory');
+INSERT INTO G_SERIE VALUES (' Fantastique','Sur ecoute');
+
+INSERT INTO NOTE_SER VALUES ('Rose Lena','Big Bang Theory',6,'sa va ','15/10/2001');
+INSERT INTO NOTE_SER VALUES ('Rose Lena','Sur ecoute',6,'sa va','15/10/2001');
+INSERT INTO NOTE_SER VALUES ('Rose Lena','Game of Thrones',6,'sa vaa','15/10/2001');
+INSERT INTO NOTE_SER VALUES ('Rose Lena','House of Cards',6,'sa va','15/10/2001');
+INSERT INTO NOTE_SER VALUES ('Rose Lena','Les Soprano',6,'sa va','15/10/2001');
+INSERT INTO NOTE_SER VALUES ('Rose Lena','Breaking Bad',3,'thetnak','15/01/2011');
+INSERT INTO NOTE_SER VALUES ('Abde ','Breaking Bad',4,'serie achekitt grave','15/01/2011');
+INSERT INTO NOTE_SER VALUES ('Abde ','Big Bang Theory',4,'serie achekitt grave','15/01/2011');
+INSERT INTO NOTE_SER VALUES ('Abde ','Game of Thrones',4,'serie achekitt grave','15/01/2011');
+INSERT INTO NOTE_SER VALUES ('Abde ','Sur ecoute',4,'serie achekitt grave','15/01/2011');
+INSERT INTO NOTE_SER VALUES ('Abde ','House of Cards',4,'serie achekittT grave','15/01/2011');
+INSERT INTO NOTE_SER VALUES (' Msw','Big Bang Theory',7,'jaime bien la serie ','11/11/2000');
+
+
+INSERT INTO PRODUCTEURS VALUES ('Alexis','Lepris','Sur ecoute');
+INSERT INTO PRODUCTEURS VALUES ('Juliette','Levant','Game of Thrones');
+INSERT INTO PRODUCTEURS VALUES ('Alexis','Lepris','Breaking Bad');
+INSERT INTO PRODUCTEURS VALUES (' Steven ','Spielberg ','Breaking Bad');
+INSERT INTO PRODUCTEURS VALUES (' Steven ','Spielberg ','Big Bang Theory');
+
+INSERT INTO SAISON VALUES (1,'Sur ecoute');
+INSERT INTO SAISON VALUES (2,'Game of Thrones');
+INSERT INTO SAISON VALUES (3,'Breaking Bad');
+INSERT INTO SAISON VALUES (4,'Breaking Bad');
+INSERT INTO SAISON VALUES (6,'Breaking Bad');
+INSERT INTO SAISON VALUES (9,'Breaking Bad');
+INSERT INTO SAISON VALUES (10,'Breaking Bad');
+INSERT INTO SAISON VALUES (13,'Breaking Bad');
+INSERT INTO SAISON VALUES (8,'Breaking Bad');
+
+
+
+INSERT INTO EPISODE VALUES ('Hiver vient',5,'15/10/2010',1,'FKDFDFDNFKJEFER','Sur ecoute');
+INSERT INTO EPISODE VALUES ('Route Royale',8,'14/01/2011',2,'jknjdnuizesjdn kqjsdenksd','Game of Thrones');
+INSERT INTO EPISODE VALUES ('Chute libre',9,'19/02/2012',3,'jknjdnuizefefenksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Le Choix ',10,'19/07/2012',3,'jkeeizefefenksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Dérapage',11,'12/06/2013',4,'jszkeeizefefenksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Vivre ou survivre',14,'14/08/2016',4,'jsdxzzkeeizefefenksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('crash',17,'02/04/2009',6,'jsdxzzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Tension',20,'02/04/2009',8,'jsdxzzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Sur le fil',17,'02/04/2009',4,'jsdxzdvergrgzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Chien et chat',25,'02/04/2009',9,'jsqqsasalko dxzzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Retour aux affaires',12,'02/04/2010',3,'jsncdsesdxzzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Prise de pouvoir',22,'02/04/2009',10,'jsdxz,ljgrtzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Kafkaïen',25,'02/04/2009',1,'jsdxz,ljgrtzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('La Mouche',26,'02/04/2009',2,'jsdngvhxz,ljgrtzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Société écran',21,'02/04/2009',13,'jsdxz,ljgrtzkeeksd','Breaking Bad');
+INSERT INTO EPISODE VALUES ('Demi-Mesures ',22,'02/04/2009',10,'jsdxz,ljgrtzkeeksd','Breaking Bad');
+
+
+
+
+
+
+INSERT INTO NOTE_EP VALUES('Hiver vient',' Kaina Fato',9,'un bon episode machalah yaa','14/02/2018');
+INSERT INTO NOTE_EP VALUES('Hiver vient','Lea Lon ',9,'il edjekndezef','15/06/2019');
+INSERT INTO NOTE_EP VALUES('Hiver vient','Cooper',8,'il edjekndezef','15/06/2019');
+INSERT INTO NOTE_EP VALUES('Le Choix ','Lea Lon ',9,'ikxz5 edjekndezejkhkjf','01/11/2019');
+INSERT INTO NOTE_EP VALUES('Vivre ou survivre',' Msw',4,'il edjekndezefjkhj yu','05/08/2017');
+
+
+INSERT INTO REALISATEURS VALUES ('Mathieu','Delaporte','Hiver vient');
+INSERT INTO REALISATEURS VALUES ('Juliette','Levant','Route Royale');
+
+
+
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Chute libre');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Le Choix ');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Dérapage');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Vivre ou survivre');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','crash');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Tension');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Sur le fil');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Chien et chat');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Retour aux affaires');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Prise de pouvoir');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Kafkaïen');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','La Mouche');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Société écran');
+INSERT INTO ACTEURS VALUES (' Bernard ',' Murat ','Demi-Mesures ');
+
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Chute libre');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Le Choix ');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Dérapage');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Vivre ou survivre');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','crash');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Tension');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Sur le fil');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Chien et chat');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Retour aux affaires');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Prise de pouvoir');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Kafkaïen');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','La Mouche');
+INSERT INTO ACTEURS VALUES (' Bryan ',' Cranston ','Société écran');
+
+
+
+INSERT INTO ACTEURS VALUES (' Serge ','Poucque  ','Le Choix ');
+INSERT INTO ACTEURS VALUES ('Juliette','Levant','Dérapage');
+INSERT INTO ACTEURS VALUES (' Gilles ','Waterkeyn ','Vivre ou survivre');
+
+
+INSERT INTO COMMEN VALUES (5,'Abde ',1,'02/05/2001','WHAAAT');
+INSERT INTO COMMEN VALUES (2,' Fati Brahimi',1,'02/05/2012','YES');
+INSERT INTO COMMEN VALUES (3,'Lea Lon ',2,'02/05/2001','HHHH');
+INSERT INTO COMMEN VALUES (4,'Cooper',5,'02/05/2014','MDDR');
+INSERT INTO COMMEN VALUES (1,'Cooper',null,'02/02/2015','U R RIGHT');
+INSERT INTO COMMEN VALUES (6,'Azrod95',null,'02/08/2016','NOOW');
+INSERT INTO COMMEN VALUES (7,'Lea Lon ',6,'02/09/2001','KABYLIE');
+INSERT INTO COMMEN VALUES (8,'Cooper',6,'08/08/2004','KABYLIE');
